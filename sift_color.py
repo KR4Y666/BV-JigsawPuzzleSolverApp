b26b6d1045170d77ba16408f50d0e20239a3ff7b
EmmaU8 <emmaurban25@gmail.com>
2022-06-06 14:53:57 +0200

    Update sift_color.py
    Add code to draw piece at the mean coordinates calculated by the sift algorithm

--- sift_color.py
+++ sift_color.py
@@ -4,10 +4,11 @@
 import cv2 
 import numpy as np
 from matplotlib import pyplot as plt
+from PIL import Image
 
 # Read in Images  
-piece_img_bgr = cv2.imread('puzzle_data/Puzzle_Piece_Eisbaer.jpg')       # puzzle piece
-template_img_bgr = cv2.imread('puzzle_data/Puzzle_Template_Eisbaer.jpg')        # puzzle template
+piece_img_bgr = cv2.imread('puzzle_data/Puzzle_Piece_Feuerwehr.jpg')       # puzzle piece
+template_img_bgr = cv2.imread('puzzle_data/Puzzle_Template_Feuerwehr.jpg')        # puzzle template
 piece_img_gray = cv2.cvtColor(piece_img_bgr, cv2.COLOR_BGR2GRAY)
 
 #Split images in color channels
@@ -39,9 +40,21 @@ def sift_algorithm(piece, template, color):
 
     # sorting out good matches and storing them
     good = []
+    x_coordinate = []
+    y_coordinate = []
     for m,n in matches:
         if m.distance < 0.7*n.distance:
             good.append(m)
+            pt1 = kp1[m.queryIdx].pt
+            pt2 = kp2[m.trainIdx].pt
+            start_x = pt1[0]
+            start_y = pt1[1]
+            target_x = pt2[0]
+            target_y = pt2[1]
+            x_coordinate.append(target_x)
+            y_coordinate.append(target_y)
+    mean_x = sum(x_coordinate)/len(x_coordinate)
+    mean_y = sum(y_coordinate)/len(y_coordinate)
 
     # Set Match Treshholds
     MIN_MATCH_COUNT = 10
@@ -67,12 +80,17 @@ def sift_algorithm(piece, template, color):
                     flags = cv2.DRAW_MATCHES_FLAGS_DEFAULT)
 
     # picture with all the matches printed      
-    return cv2.drawMatches(piece,kp1,template,kp2,good,None,**draw_params)
+    return mean_x, mean_y, cv2.drawMatches(piece,kp1,template,kp2,good,None,**draw_params)
+
+#return values of sift algorithm
+result_sift_blue = sift_algorithm(piece_img_blue, template_img_blue, (255,0,0))
+result_sift_green = sift_algorithm(piece_img_green, template_img_green, (0,255,0))
+result_sift_red = sift_algorithm(piece_img_red, template_img_red, (0,0,255))
 
 #result images after applying the sift algorithm
-result_img_blue = sift_algorithm(piece_img_blue, template_img_blue, (255,0,0))
-result_img_green = sift_algorithm(piece_img_green, template_img_green, (0,255,0))
-result_img_red = sift_algorithm(piece_img_red, template_img_red, (0,0,255))
+result_img_blue = result_sift_blue[2]
+result_img_green = result_sift_green[2]
+result_img_red = result_sift_red[2]
 
 result_img_blue = result_img_blue[:,:,0]
 result_img_green = result_img_green[:,:,1]
@@ -82,5 +100,26 @@ result_img_red = result_img_red[:,:,2]
 result_img = cv2.merge([result_img_blue, result_img_green, result_img_red])
 
 #show final image (converted to RGB)
-plt.imshow(cv2.cvtColor(result_img, cv2.COLOR_BGR2RGB))
+#plt.imshow(cv2.cvtColor(result_img, cv2.COLOR_BGR2RGB))
+#plt.show()
+
+#calculate mean coordinates of piece in template
+mean_x_blue = result_sift_blue[0]
+mean_y_blue = result_sift_blue[1]
+mean_x_green = result_sift_green[0]
+mean_y_green = result_sift_green[1]
+mean_x_red = result_sift_red[0]
+mean_y_red = result_sift_red[1]
+mean_x = int((mean_x_blue + mean_x_green + mean_x_red)/3)
+mean_y = int((mean_y_blue + mean_y_green + mean_y_red)/3)
+print(mean_x, mean_y)
+
+#show piece at its mean coordinates on the puzzle template
+im1 = Image.open('puzzle_data/Puzzle_Piece_Feuerwehr.jpg')
+im2 = Image.open('puzzle_data/Puzzle_Template_Feuerwehr.jpg')
+back_im = im2.copy()
+im1 = im1.resize((400,400)) #TODO image resizing
+back_im.paste(im1, (mean_x, mean_y))
+plt.imshow(back_im)
 plt.show()
+
