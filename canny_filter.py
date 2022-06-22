4a578658e335c621498188055454035e334aff5f
JensNiebling <Jenss232@googlemail.com>
2022-06-22 18:15:02 +0200

    Update canny_filter.py

--- canny_filter.py
+++ canny_filter.py
@@ -2,7 +2,7 @@
 import cv2 
 import numpy as np
 
-frame = cv2.imread('puzzle_data/Puzzle_Piece_Feuerwehr.jpg') 
+frame = cv2.imread('puzzle_data/puzzle6_1.jpg') 
 # Converting the image to grayscale.
 gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
@@ -10,7 +10,6 @@ gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 edges = cv2.Canny(gray, 20, 30)
 # Using the Canny filter with different parameters
 edges_high_thresh = cv2.Canny(gray, 60, 120)
-sobelxy = cv2.Sobel(frame, ddepth=cv2.CV_64F, dx=1, dy=1, ksize=5) 
 edges_high_thresh2 = cv2.Canny(gray, 25, 200)
 
 
@@ -22,9 +21,5 @@ images = np.hstack((gray, edges, edges_high_thresh, edges_high_thresh2))
 cv2.imshow('Frame', images)
 
 cv2.waitKey(0) 
-cv2.imshow('Frame2', sobelxy)
-
-cv2.waitKey(0) 
-  
 #closing all open windows 
 cv2.destroyAllWindows() 
