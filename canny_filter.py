51586a80fe17eec84fb2ef1688da0460a38a9772
JensNiebling <Jenss232@googlemail.com>
2022-06-22 15:37:41 +0200

    Canny-Filter

---
+++ canny_filter.py
@@ -0,0 +1,30 @@
+# Imports
+import cv2 
+import numpy as np
+
+frame = cv2.imread('puzzle_data/Puzzle_Piece_Feuerwehr.jpg') 
+# Converting the image to grayscale.
+gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+# Using the Canny filter to get contours
+edges = cv2.Canny(gray, 20, 30)
+# Using the Canny filter with different parameters
+edges_high_thresh = cv2.Canny(gray, 60, 120)
+sobelxy = cv2.Sobel(frame, ddepth=cv2.CV_64F, dx=1, dy=1, ksize=5) 
+edges_high_thresh2 = cv2.Canny(gray, 25, 200)
+
+
+# Stacking the images to print them together
+# For comparison
+images = np.hstack((gray, edges, edges_high_thresh, edges_high_thresh2))
+
+# Display the resulting frame
+cv2.imshow('Frame', images)
+
+cv2.waitKey(0) 
+cv2.imshow('Frame2', sobelxy)
+
+cv2.waitKey(0) 
+  
+#closing all open windows 
+cv2.destroyAllWindows() 
